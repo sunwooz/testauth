11d2ad8878e6c2bf1551411a27c401113331980c
Sunwoo Yang <yangsunwoo@gmail.com>
2012-04-08 15:29:41 -0400

    removed gem.lock

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Testauth::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -42,8 +42,8 @@ Testauth::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  #Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
