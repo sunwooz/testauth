f62a2734166766f197523db7c568bc828362db97
Sunwoo Yang <yangsunwoo@gmail.com>
2012-04-20 01:26:47 -0400

    removed json from beta create

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -7,10 +7,8 @@ class BetaController < ApplicationController
         BetaMailer.beta_email(@beta).deliver
         
         format.html { redirect_to(root_url, :notice => 'You have successfully joined the beta.')}
-        format.json { render :json => @beta, :status => :created, :location => @beta }
       else
         format.html { render :action => 'new' }
-        format.json { render :json => @beta.errors, :status => :unprocessable_entity }
       end
     end
   end
