1443fffcf7ec1a8322e4294f1fa1aeab430046d7
Sunwoo Yang <yangsunwoo@gmail.com>
2012-04-20 12:02:58 -0400

    beta validations and add_index :beta, :email

--- app/models/beta.rb
+++ app/models/beta.rb
@@ -1,3 +1,13 @@
 class Beta < ActiveRecord::Base
   attr_accessible :email
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  
+  validates :email, :presence => { :message => "Email Field Cannot Be Blank!" },
+                    :length => {
+                      :maximum => 50,
+                      :minimum => 5
+                    },
+                    :format => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418014841) do
+ActiveRecord::Schema.define(:version => 20120420152906) do
 
   create_table "beta", :force => true do |t|
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "beta", ["email"], :name => "index_beta_on_email", :unique => true
 
   create_table "posts", :force => true do |t|
     t.string   "title"
