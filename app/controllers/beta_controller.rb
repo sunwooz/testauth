cb60b50d2a1b508e83415a28516ab954538ff919
Sunwoo Yang <yangsunwoo@gmail.com>
2012-04-20 02:41:23 -0400

    changed json to root_url

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -7,7 +7,7 @@ class BetaController < ApplicationController
         BetaMailer.beta_email(@beta).deliver
         
         format.html { redirect_to(root_url, :notice => 'You have successfully joined the beta.')}
-        format.json { render :json => @beta, :status => :created, :location => @beta }
+        format.json { render :json => root_url, :status => :created, :location => root_url }
       else
         format.html { render :action => 'new' }
         format.json { render :json => @beta.errors, :status => :unprocessable_entity }
